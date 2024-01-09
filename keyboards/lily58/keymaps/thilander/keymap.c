832ba98bda07f3a1702230b1f943e973e096808f
Andreas Thilander <andreas@thilander.net>
2024-01-09 22:24:23 +0100

    switch rise/lower layers

--- keyboards/lily58/keymaps/thilander/keymap.c
+++ keyboards/lily58/keymaps/thilander/keymap.c
@@ -17,26 +17,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,   KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
                             KC_LGUI, MO(_LOWER), KC_LALT, KC_SPC,      KC_ENT, KC_BSPC, MO(_RAISE), TG(_QWERTY_SWE)
     ),
-    [_LOWER] = LAYOUT(
+    [_RAISE] = LAYOUT(
         KC_ESC , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,   KC_7,    KC_8,    KC_9,    KC_TRNS, KC_LBRC,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,   KC_4,    KC_5,    KC_6,    KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS,   KC_1,    KC_2,    KC_3,    KC_TRNS, KC_RSFT,
-                                KC_TRNS, KC_TRNS, MO(_LOWER), KC_TRNS, KC_TRNS, MO(_RAISE), KC_DEL, KC_TRNS
+                                KC_TRNS, MO(_LOWER), KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, MO(_RAISE), KC_TRNS
     ),
-    [_RAISE] = LAYOUT(
+    [_LOWER] = LAYOUT(
         KC_ESC , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5 ,                    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_UP  , KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN , KC_END, KC_TRNS,                   KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                KC_TRNS, KC_TRNS, MO(_LOWER), KC_TRNS, KC_TRNS, MO(_RAISE), KC_BSPC, KC_TRNS
+                                KC_TRNS, MO(_LOWER), KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, MO(_RAISE), KC_TRNS
     ),
     [_ADJUST] = LAYOUT(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX,           XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, KC_KB_VOLUME_UP,   XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, KC_KB_VOLUME_DOWN, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_KB_MUTE,        XXXXXXX, XXXXXXX, XXXXXXX,
-                                    KC_TRNS, KC_TRNS, MO(_LOWER), KC_TRNS, KC_TRNS, MO(_RAISE), KC_BSPC, KC_TRNS
+                                    KC_TRNS, MO(_LOWER), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_RAISE), KC_TRNS
     ),
     [_QWERTY_SWE] = LAYOUT(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
