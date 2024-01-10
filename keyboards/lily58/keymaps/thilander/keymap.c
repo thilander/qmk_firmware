9229054c7f9dee94b9ffe8a8fb5758f67b9f270e
Andreas Thilander <andreas@thilander.net>
2024-01-10 19:15:32 +0100

    Move modifiers

--- keyboards/lily58/keymaps/thilander/keymap.c
+++ keyboards/lily58/keymaps/thilander/keymap.c
@@ -15,28 +15,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
         KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,   KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-                            KC_LGUI, MO(_LOWER), KC_LALT, KC_SPC,      KC_ENT, KC_BSPC, MO(_RAISE), TG(_QWERTY_SWE)
-    ),
-    [_RAISE] = LAYOUT(
-        KC_ESC , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,   KC_7,    KC_8,    KC_9,    KC_TRNS, KC_LBRC,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,   KC_4,    KC_5,    KC_6,    KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS,   KC_1,    KC_2,    KC_3,    KC_TRNS, KC_RSFT,
-                                KC_TRNS, MO(_LOWER), KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, MO(_RAISE), KC_TRNS
+                            KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC,      KC_ENT, MO(_RAISE), KC_BSPC, TG(_QWERTY_SWE)
     ),
     [_LOWER] = LAYOUT(
         KC_ESC , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5 ,                    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_UP  , KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN , KC_END, KC_TRNS,                   KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                KC_TRNS, MO(_LOWER), KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, MO(_RAISE), KC_TRNS
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS,
+                                KC_TRNS, KC_TRNS, MO(_LOWER), KC_TRNS, KC_TRNS, MO(_RAISE), KC_DEL, KC_TRNS
+    ),
+    [_RAISE] = LAYOUT(
+        KC_ESC , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,   KC_7,    KC_8,    KC_9,    KC_TRNS, KC_LBRC,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,   KC_4,    KC_5,    KC_6,    KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS,   KC_1,    KC_2,    KC_3,    KC_TRNS, KC_BSLS,
+                                KC_TRNS, KC_TRNS, MO(_LOWER), KC_TRNS, KC_TRNS, MO(_RAISE), KC_DEL, KC_TRNS
     ),
     [_ADJUST] = LAYOUT(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX,           XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, KC_KB_VOLUME_UP,   XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, KC_KB_VOLUME_DOWN, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_KB_MUTE,        XXXXXXX, XXXXXXX, XXXXXXX,
-                                    KC_TRNS, MO(_LOWER), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_RAISE), KC_TRNS
+                                    KC_TRNS, KC_TRNS, MO(_LOWER), KC_TRNS, KC_TRNS, MO(_RAISE), KC_TRNS, KC_TRNS
     ),
     [_QWERTY_SWE] = LAYOUT(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-    ),
+    )
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
